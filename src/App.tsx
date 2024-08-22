1c42f7bdc819527be218b887e0cff54a3c55c21e
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-21 23:39:04 -0300

    Feature: Change color theme by specifying color in the data.json file

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import Category from "./components/Category/Category";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    // Assign the CSS variable (--color-theme) with the value in data.json
+    document.documentElement.style.setProperty(
+      "--color-theme",
+      data.colorTheme,
+    );
+  }, []);
+
   return (
     <div className="app__container">
       <Header name={data.header.name} resume={data.header.resume} />
